801216aee8cd811af25fb67e48e7cebd1e4147c7
Yriy Radygin <48766782+YRadygin@users.noreply.github.com>
2022-08-24 11:46:43 +0300

    Add files via upload

---
+++ StopEnemy.dpr
@@ -0,0 +1,14 @@
+program StopEnemy;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'C:\Users\DarkSlayer\Downloads\Projects\Projects\Unit1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
